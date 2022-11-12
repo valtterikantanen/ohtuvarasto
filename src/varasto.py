e868e542f520ed1afbea573ca5ec4d95dea453cb
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-12 05:49:29 +0200

    Add Pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,10 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        # nollataan virheellinen tilavuus
+        self.tilavuus = max(0.0, tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -16,7 +13,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
